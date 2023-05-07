67cac3d2e23e60f09088983a718bb3ded958357c
Navnath parte <9nath.parte@gmail.com>
2023-05-07 18:25:42 +0530

    done feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeService } from './service/employee.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import * as moment from 'moment';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-crud-app';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'firstName',
+    'lastName',
+    'email',
+    'dob',
+    'gender',
+    'education',
+    'company',
+    'experience',
+    'package',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<any>;
 
-  constructor(private _dialog: MatDialog) {}
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private _dialog: MatDialog,
+    private _empService: EmployeeService
+  ) {}
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
 
   employeeForm() {
-    this._dialog.open(EmployeeComponent)
+    this._dialog.open(EmployeeComponent);
+    this.getEmployeeList();
+  }
+
+  getEmployeeList() {
+    this._empService.getEmployeeList().subscribe({
+      next: (data: any) => {
+        const res = data.data.map((elt: any) => {
+          return {
+            ...elt,
+            dob: moment(elt.dob).format('DD-MM-YYYY'),
+          };
+        });
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: console.log,
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteEmployee(id: string) {
+    this._empService.delateEmployee(id).subscribe({
+      next: (res) => {
+        alert('delete Successfully!');
+        this.getEmployeeList();
+      },
+      error: console.log,
+    });
+  }
+
+  updateEmployee(id: string, data: string) {
+    this._empService.updateEmployee(id, data).subscribe({
+      next: (res) => {
+        alert('updated Successfully!');
+      },
+      error: console.log,
+    });
   }
 }
--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +7,19 @@ import { Observable } from 'rxjs';
 export class EmployeeService {
   constructor(private _http: HttpClient) {}
 
-  addEmployee(data: any): Observable<any> {
-    return this._http.post('http://localhost:3000/employee', data);
+  addEmployee(data: any) {
+    return this._http.post('http://localhost:3000/save', data);
+  }
+
+  getEmployeeList() {
+    return this._http.get('http://localhost:3000/employee');
+  }
+
+  delateEmployee(id: string) {
+    return this._http.delete(`http://localhost:3000/${id}`);
+  }
+
+  updateEmployee(id: string, data: any) {
+    return this._http.put(`http://localhost:3000/update/${id}`, data);
   }
 }
